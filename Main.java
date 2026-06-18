21a64b240391695ee82f8e845d34ef1b78802b15
PugPapa0 <becktrav3@icloud.com>
2026-06-18 09:50:47 -0700

    Declaration statements added, Initialization statements added, Print output added

--- Main.java
+++ Main.java
@@ -1,22 +1,36 @@
-// YOUR NAME: 
-// COLLABORATORS:  
-// DATE: 
+// YOUR NAME: Travis Becker
+// COLLABORATORS:  N/A
+// DATE: 6/18/2026
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		// DECLARATION SECTION
+		double avgDacaTaxContribution;
+		int avgDacaTaxContributionRounded;
+		String stateMostDaca;
 
 		// INITIALIZATION SECTION
-		
+		char stateMostDaca1 = 'a';
+		char stateMostDaca2 = 'c';
+		int numDacaRecipient = 700000;
+		long dacaFederalTaxRevenue = 60000000000L;
+
 		// INPUT SECTION
 		// N/A (no input for this lab)
-		
-		// CALCULATION & PROCESSING SECTION
 
-		
+		// CALCULATION & PROCESSING SECTION
+		avgDacaTaxContribution = (double) dacaFederalTaxRevenue / numDacaRecipient;
+		avgDacaTaxContributionRounded = (int) avgDacaTaxContribution;
+		stateMostDaca1 = (char) (stateMostDaca1 - 32);
+		stateMostDaca2 = (char) (stateMostDaca2 - 32);
+		stateMostDaca = "" + stateMostDaca2 + stateMostDaca1;
+
 		// OUTPUT SECTION
+		System.out.println("Average DACA-recipient Tax Contribution: $" + avgDacaTaxContribution);
+		System.out.println("Rounded down to nearest whole dollar: $" + avgDacaTaxContributionRounded);
+		System.out.println("State with most DACA recipients: " + stateMostDaca);
 		
 	}
 }
